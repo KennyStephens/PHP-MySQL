b3244730f3f4d0df0323e556c55cc9fd1c955210
Kenny Stephens <kennethcstephens@gmail.com>
2018-09-08 19:08:36 -0600

    Quick fix.

--- 06 Securing the Application/navbar.php
+++ 06 Securing the Application/navbar.php
@@ -5,7 +5,7 @@
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="index.php">Display</a>
+        <a class="nav-link" href="index.php">Blog</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="newEntry.php">Make a Comment</a>
