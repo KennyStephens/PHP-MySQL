c281468aad79670a933da1ad6b5c03d94e9d0a4b
Kenny Stephens <kennethcstephens@gmail.com>
2018-09-10 14:58:44 -0600

    Some changes.

--- 05 Managing Records/index.php
+++ 05 Managing Records/index.php
@@ -16,13 +16,21 @@ include_once('navbar.php');
 <h1>Employee Directory</h1>
 
 <?php
+$photo = $_POST['photo'];
+// Make photo path and name
+$ext = pathinfo( $_FILES['photo']['name'], PATHINFO_EXTENSION);
+$filename = $first. $last.time().'.'.$ext.'';
+$filepath =  'employees/';
+
 // connection
 $dbconnection = mysqli_connect('localhost','thedevf5_3760use','I22slh#DQCU','thedevf5_3760test') or die ('Connection to the database failed.');
 // build query
 $query = "SELECT * FROM employee_simple ORDER BY last ASC";
 // send to database
 $result = mysqli_query($dbconnection, $query) or die ('query failed');
+
 while($row = mysqli_fetch_array($result)) {
+
   echo $row['last'].', '. $row['first'].' - '.$row['dept'];
   echo '<a href="update.php?id='.$row['id'].'"> - Update </a>';
   echo '</p>';
