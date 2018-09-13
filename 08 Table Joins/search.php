abf13de1d300709b010e06b17a326d75221087b3
Kenny Stephens <34525009+KennyStephens@users.noreply.github.com>
2018-09-13 13:39:24 -0600

    Started working on search page.

---
+++ 08 Table Joins/search.php
@@ -0,0 +1,61 @@
+<?php
+require_once('variables.php');
+
+  // Build connection to database
+  $dbconnection = mysqli_connect(HOST,USER,PASSWORD,DB_NAME) or die ('Connection to the database failed.');
+
+// Get the emphasis from database
+$query = "SELECT * FROM dgm_emphasis ORDER BY value";
+
+// talk to database
+$result = mysqli_query($dbconnection, $query) or die ('Query failed');
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>Search</title>
+  </head>
+
+  <body style="background: #E0EAFC;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to right, #CFDEF3, #E0EAFC);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to right, #CFDEF3, #E0EAFC); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */">
+<?php include_once('navbar.php');   ?>
+    <div class="container">
+    <h1 class="mt-3">Search</h1>
+ <ul class="list-group">
+    <?php  
+    while($row = mysqli_fetch_array($result)) {
+        echo '<li class="list-group-item"><a href="index.php?id='.$row['emphasis_id'].'">';
+        echo $row['value'];
+        echo '</a></li>';
+    };
+    
+    ?>
+ </ul>
+      
+
+      
+    </div> <!--end of container-->
+
+  <?php mysqli_close($dbconnection); ?>
+  
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
